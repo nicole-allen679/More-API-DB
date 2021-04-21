0a395084a02819e109719deb6064b176ccca0684
nicole-allen679 <nicole.allen679@gmail.com>
2021-04-21 15:34:40 -0400

    all api and db working

--- functions/src/tasks/index.js
+++ functions/src/tasks/index.js
@@ -1,7 +1,37 @@
+const admin = require('firebase-admin')
+
+const serviceAccount = require('../../credentials.json')
+
+let db;
+
+function connectToFirestore() {
+  if (!db) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+    db = admin.firestore()
+  }
+}
 exports.getTasks = (req, res) => {
-    res.send('Here are the tasks')
+  connectToFirestore()
+  db.collection('tasks')
+    .get()
+    .then((collection) => {
+      let ourTasks = []
+      collection.forEach((doc) => {
+        let thisTask = doc.data()
+        thisTask.id = doc.id
+        ourTasks.push(thisTask)
+      })
+      res.send(ourTasks)
+    })
+    .catch((err) => res.status(500).send('Error getting tasks' + err.message))
 }
 
 exports.createTask = (req, res) => {
-    res.send('nice task!')
+  connectToFirestore()
+  const newTask = req.body
+  db.collection('tasks').add(newTask)
+  .then(() => this.getTasks(req, res))
+  .catch((err) => res.status(500).send('Error creating tasks' + err.message))
 }
